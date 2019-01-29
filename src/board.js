b671928fb59c4440558293e6402720c177046e7b
Josh Morrow <josh@jcmorrow.com>
2019-01-29 09:20:46 -0500

    Get function declarations out of constructors
    I really love using arrow syntax to define functions inside of classes,
    but I *cannot* for the life of me get that babel transform (https://babeljs.io/docs/en/babel-plugin-proposal-class-properties)
     to work. I don't like how in the previous way I had to define every
     method in the constructor, so I'm going back to the old `bind` method.
     Hopefully one day that proposal will just be a part of the next
     standard.

--- src/board.js
+++ src/board.js
@@ -7,152 +7,19 @@ class Board {
     this.rowCount = rowCount;
     this.selected = false;
 
-    this.fillInColumn = column => {
-      let newColumn = column.filter(space => space);
-      for (let i = column.length - newColumn.length; i > 0; i--) {
-        newColumn.unshift(randomColor());
-      }
-      return newColumn;
-    };
-
-    this.fillIn = () => {
-      this.columns = this.columns.map(column => this.fillInColumn(column));
-    };
-
-    this.handleSelect = (column, row) => {
-      if (
-        column >= this.rowCount ||
-        row >= this.rowCount ||
-        row < 0 ||
-        column < 0
-      ) {
-        return;
-      }
-
-      if (!this.selected) {
-        this.selected = [column, row];
-      } else {
-        if (this.adjacent([column, row], this.selected)) {
-          this.swap([column, row], this.selected);
-        }
-        this.selected = false;
-      }
-    };
-
-    this.adjacent = (a, b) => {
-      if (a[0] === b[0]) {
-        return differentByOne(a[1], b[1]);
-      } else if (a[1] === b[1]) {
-        return differentByOne(a[0], b[0]);
-      }
-      return false;
-    };
-
-    this.pivotCoordinates = coordinates => {
-      return [coordinates[1], coordinates[0]];
-    };
-
-    this.checkForExplosions = (columns, vertical = false) => {
-      let lastColor = false;
-      let currentMatch = [];
-      const matches = [];
-
-      const appendToMatches = match => {
-        if (match.length >= MINIMUM_EXPLOSION_SIZE) {
-          if (vertical) {
-            match = match.map(this.pivotCoordinates);
-          }
-          matches.push(match);
-        }
-      };
-
-      columns.map((column, columnIndex) => {
-        lastColor = false;
-        currentMatch = [];
-        column.map((row, rowIndex) => {
-          if (lastColor === false) {
-            // first jewel of the row
-            lastColor = row;
-            currentMatch.push([columnIndex, rowIndex]);
-            return;
-          } else {
-            // not first jewel of the row, which means we have comparing to do
-            if (lastColor === row) {
-              // we are still matching so far!
-              currentMatch.push([columnIndex, rowIndex]);
-            } else {
-              // our match has ended! Let's try and add everything that has
-              // matched so far and restart
-              appendToMatches(currentMatch);
-              currentMatch = [[columnIndex, rowIndex]];
-              lastColor = row;
-            }
-          }
-          if (rowIndex === this.rowCount - 1) {
-            // last jewel of the row, let's try and add a match no matter what!
-            appendToMatches(currentMatch);
-          }
-        });
-      });
-
-      return matches;
-    };
-
-    this.pivot = () => {
-      const pivoted = new Array(this.rowCount)
-        .fill(undefined)
-        .map(() => new Array(this.rowCount).fill(undefined));
-      for (let row = 0; row < this.rowCount; row++) {
-        for (let col = 0; col < this.rowCount; col++) {
-          pivoted[col][row] = this.columns[row][col];
-        }
-      }
-      return pivoted;
-    };
-
-    this.checkHorizontal = () => this.checkForExplosions(this.columns);
-
-    this.checkVertical = () => this.checkForExplosions(this.pivot(), 1);
-
-    this.resolveAllExplosions = () => {
-      let exploded = false;
-      while (this.checkHorizontal().length || this.checkVertical().length) {
-        exploded = true;
-        this.resolveExplosions(this.checkHorizontal());
-        this.resolveExplosions(this.checkVertical());
-      }
-      return exploded;
-    };
-
-    this.resolveExplosions = explosions => {
-      explosions.forEach(explosion => {
-        explosion.forEach(explosionSpace => {
-          this.columns[explosionSpace[0]][explosionSpace[1]] = false;
-        });
-      });
-      this.fillIn();
-    };
-
-    this.swap = (a, b) => {
-      const tmpA = this.columns[a[0]][a[1]];
-      this.columns[a[0]][a[1]] = this.columns[b[0]][b[1]];
-      this.columns[b[0]][b[1]] = tmpA;
-      if (!this.resolveAllExplosions()) {
-        // if nothing exploded, swap back
-        this.columns[b[0]][b[1]] = this.columns[a[0]][a[1]];
-        this.columns[a[0]][a[1]] = tmpA;
-      }
-    };
-
-    this.areNeighbors = (a, b) => {
-      if (a[0] === b[0]) {
-        return differentByOne(a[1], b[1]);
-      }
-      if (a[1] === b[1]) {
-        return differentByOne(a[0], b[0]);
-      }
-      return false;
-    };
+    this.adjacent = this.adjacent.bind(this);
+    this.areNeighbors = this.areNeighbors.bind(this);
+    this.checkForExplosions = this.checkForExplosions.bind(this);
+    this.checkHorizontal = this.checkHorizontal.bind(this);
+    this.checkVertical = this.checkVertical.bind(this);
+    this.fillIn = this.fillIn.bind(this);
+    this.fillInColumn = this.fillInColumn.bind(this);
+    this.handleSelect = this.handleSelect.bind(this);
+    this.pivot = this.pivot.bind(this);
+    this.pivotCoordinates = this.pivotCoordinates.bind(this);
+    this.resolveAllExplosions = this.resolveAllExplosions.bind(this);
+    this.resolveExplosions = this.resolveExplosions.bind(this);
+    this.swap = this.swap.bind(this);
 
     // we create the board for the first time by randomly filling it and then
     // resolving any explosions that got created by the randomness
@@ -163,6 +30,157 @@ class Board {
       );
 
     this.resolveAllExplosions();
+  }
+
+  fillInColumn(column) {
+    let newColumn = column.filter(space => space);
+    for (let i = column.length - newColumn.length; i > 0; i--) {
+      newColumn.unshift(randomColor());
+    }
+    return newColumn;
+  }
+
+  fillIn() {
+    this.columns = this.columns.map(column => this.fillInColumn(column));
+  }
+
+  handleSelect(column, row) {
+    if (
+      column >= this.rowCount ||
+      row >= this.rowCount ||
+      row < 0 ||
+      column < 0
+    ) {
+      return;
+    }
+
+    if (!this.selected) {
+      this.selected = [column, row];
+    } else {
+      if (this.adjacent([column, row], this.selected)) {
+        this.swap([column, row], this.selected);
+      }
+      this.selected = false;
+    }
+  }
+
+  adjacent(a, b) {
+    if (a[0] === b[0]) {
+      return differentByOne(a[1], b[1]);
+    } else if (a[1] === b[1]) {
+      return differentByOne(a[0], b[0]);
+    }
+    return false;
+  }
+
+  pivotCoordinates(coordinates) {
+    return [coordinates[1], coordinates[0]];
+  }
+
+  checkForExplosions(columns, vertical = false) {
+    let lastColor = false;
+    let currentMatch = [];
+    const matches = [];
+
+    const appendToMatches = match => {
+      if (match.length >= MINIMUM_EXPLOSION_SIZE) {
+        if (vertical) {
+          match = match.map(this.pivotCoordinates);
+        }
+        matches.push(match);
+      }
+    };
+
+    columns.map((column, columnIndex) => {
+      lastColor = false;
+      currentMatch = [];
+      column.map((row, rowIndex) => {
+        if (lastColor === false) {
+          // first jewel of the row
+          lastColor = row;
+          currentMatch.push([columnIndex, rowIndex]);
+          return;
+        } else {
+          // not first jewel of the row, which means we have comparing to do
+          if (lastColor === row) {
+            // we are still matching so far!
+            currentMatch.push([columnIndex, rowIndex]);
+          } else {
+            // our match has ended! Let's try and add everything that has
+            // matched so far and restart
+            appendToMatches(currentMatch);
+            currentMatch = [[columnIndex, rowIndex]];
+            lastColor = row;
+          }
+        }
+        if (rowIndex === this.rowCount - 1) {
+          // last jewel of the row, let's try and add a match no matter what!
+          appendToMatches(currentMatch);
+        }
+      });
+    });
+
+    return matches;
+  }
+
+  pivot() {
+    const pivoted = new Array(this.rowCount)
+      .fill(undefined)
+      .map(() => new Array(this.rowCount).fill(undefined));
+    for (let row = 0; row < this.rowCount; row++) {
+      for (let col = 0; col < this.rowCount; col++) {
+        pivoted[col][row] = this.columns[row][col];
+      }
+    }
+    return pivoted;
+  }
+
+  checkHorizontal() {
+    this.checkForExplosions(this.columns);
+  }
+
+  checkVertical() {
+    this.checkForExplosions(this.pivot(), 1);
+  }
+
+  resolveAllExplosions() {
+    let exploded = false;
+    while (this.checkHorizontal().length || this.checkVertical().length) {
+      exploded = true;
+      this.resolveExplosions(this.checkHorizontal());
+      this.resolveExplosions(this.checkVertical());
+    }
+    return exploded;
+  }
+
+  resolveExplosions(explosions) {
+    explosions.forEach(explosion => {
+      explosion.forEach(explosionSpace => {
+        this.columns[explosionSpace[0]][explosionSpace[1]] = false;
+      });
+    });
+    this.fillIn();
+  }
+
+  swap(a, b) {
+    const tmpA = this.columns[a[0]][a[1]];
+    this.columns[a[0]][a[1]] = this.columns[b[0]][b[1]];
+    this.columns[b[0]][b[1]] = tmpA;
+    if (!this.resolveAllExplosions()) {
+      // if nothing exploded, swap back
+      this.columns[b[0]][b[1]] = this.columns[a[0]][a[1]];
+      this.columns[a[0]][a[1]] = tmpA;
+    }
+  }
+
+  areNeighbors(a, b) {
+    if (a[0] === b[0]) {
+      return differentByOne(a[1], b[1]);
+    }
+    if (a[1] === b[1]) {
+      return differentByOne(a[0], b[0]);
+    }
+    return false;
   }
 }
 
--- src/canvas-renderer.js
+++ src/canvas-renderer.js
@@ -1,4 +1,5 @@
 import { COLORS } from "./utilities";
+const CANVAS_WIDTH = 500;
 const CUBE_SIZE = 50;
 
 class CanvasRenderer {
@@ -7,61 +8,66 @@ class CanvasRenderer {
     this.canvas = document.getElementById("bejeweled");
     this.context = this.canvas.getContext("2d");
 
-    this.drawCube = (column, row, color) => {
-      this.context.save();
-      this.context.fillStyle = color || "black";
-      const originX = row * CUBE_SIZE;
-      const originY = column * CUBE_SIZE;
-      this.context.translate(originX, originY);
-      this.context.rotate((Math.PI / 180) * 45);
-      this.context.fillRect(
-        0.5 * CUBE_SIZE,
-        -0.5 * CUBE_SIZE,
-        0.6 * CUBE_SIZE,
-        0.6 * CUBE_SIZE
-      );
-      this.context.restore();
-    };
-
-    this.drawOutline = (column, row) => {
-      this.context.save();
-      this.context.fillStyle = "black";
-      const originX = row * CUBE_SIZE;
-      const originY = column * CUBE_SIZE;
-      this.context.translate(originX, originY);
-      this.context.rotate((Math.PI / 180) * 45);
-      this.context.strokeRect(
-        0.5 * CUBE_SIZE,
-        -0.5 * CUBE_SIZE,
-        0.6 * CUBE_SIZE,
-        0.6 * CUBE_SIZE
-      );
-      this.context.restore();
-    };
-
-    this.drawBoard = () => {
-      this.context.clearRect(0, 0, 500, 500);
-      this.board.columns.map((column, columnIndex) => {
-        column.map((row, rowIndex) => {
-          this.drawCube(rowIndex, columnIndex, COLORS[row]);
-        });
-      });
-      if (this.board.selected) {
-        this.drawOutline(this.board.selected[1], this.board.selected[0]);
-      }
-      window.requestAnimationFrame(this.drawBoard);
-    };
-
-    this.handleClick = e => {
-      const rowIndex = Math.floor(e.offsetX / CUBE_SIZE);
-      const columnIndex = Math.floor(e.offsetY / CUBE_SIZE);
-
-      board.handleSelect(rowIndex, columnIndex);
-    };
+    this.drawCube = this.drawCube.bind(this);
+    this.drawBoard = this.drawBoard.bind(this);
+    this.drawOutline = this.drawOutline.bind(this);
+    this.handleClick = this.handleClick.bind(this);
 
     this.canvas.onclick = this.handleClick;
 
     window.requestAnimationFrame(this.drawBoard);
+  }
+
+  drawCube(column, row, color) {
+    this.context.save();
+    this.context.fillStyle = color || "black";
+    const originX = row * CUBE_SIZE;
+    const originY = column * CUBE_SIZE;
+    this.context.translate(originX, originY);
+    this.context.rotate((Math.PI / 180) * 45);
+    this.context.fillRect(
+      0.5 * CUBE_SIZE,
+      -0.5 * CUBE_SIZE,
+      0.5 * CUBE_SIZE,
+      0.5 * CUBE_SIZE
+    );
+    this.context.restore();
+  }
+
+  drawOutline(column, row) {
+    this.context.save();
+    this.context.fillStyle = "black";
+    const originX = row * CUBE_SIZE;
+    const originY = column * CUBE_SIZE;
+    this.context.translate(originX, originY);
+    this.context.rotate((Math.PI / 180) * 45);
+    this.context.strokeRect(
+      0.5 * CUBE_SIZE,
+      -0.5 * CUBE_SIZE,
+      0.5 * CUBE_SIZE,
+      0.5 * CUBE_SIZE
+    );
+    this.context.restore();
+  }
+
+  drawBoard() {
+    this.context.clearRect(0, 0, CANVAS_WIDTH, CANVAS_WIDTH);
+    this.board.columns.map((column, columnIndex) => {
+      column.map((row, rowIndex) => {
+        this.drawCube(rowIndex, columnIndex, COLORS[row]);
+      });
+    });
+    if (this.board.selected) {
+      this.drawOutline(this.board.selected[1], this.board.selected[0]);
+    }
+    window.requestAnimationFrame(this.drawBoard);
+  }
+
+  handleClick(e) {
+    const rowIndex = Math.floor(e.offsetX / CUBE_SIZE);
+    const columnIndex = Math.floor(e.offsetY / CUBE_SIZE);
+
+    this.board.handleSelect(rowIndex, columnIndex);
   }
 }
 
